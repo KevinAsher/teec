3ef6ce2fe647e6cd830bcdd00850c347f01958ac
Kevin Asher <kev_asher@hotmail.com>
2020-05-31 09:27:50 -0300

    test other filters

--- testbench/testbench_fir_filter.vhd
+++ testbench/testbench_fir_filter.vhd
@@ -14,24 +14,26 @@ end entity;
 
 architecture arch of testbench_fir_filter is
   constant period : time := 10 us;
-  signal rstn : std_logic := '0';
   signal clk : std_logic := '1';
   
-  file fil_in : text;
-  file fil_out : text;
+  file f_FILE_IN : text;
+  file f_FILE_OUT : text;
 
-  signal valid : std_logic := '0';
   signal w_PIX_IN   : std_logic_vector(7 downto 0);
   signal w_PIX_OUT   : std_logic_vector(7 downto 0);
   
-  signal valid_o : std_logic;
-  signal pix_o : std_logic_vector(7 downto 0);
-
+  signal w_VALID_PIX: std_logic;
   constant c_WINDOW_SIZE : integer := 3;
   signal w_KERNEL : t_SHIFT_REGISTER(c_WINDOW_SIZE*c_WINDOW_SIZE-1 downto 0) := (
-    X"01", X"01", X"01",
-    X"01", X"01", X"01",
-    X"01", X"01", X"01"
+    -- Box Blur
+    -- X"01", X"01", X"01",
+    -- X"01", X"01", X"01",
+    -- X"01", X"01", X"01"
+
+    -- Edge Dectection
+    X"FF", X"FF", X"FF",
+    X"FF", X"08", X"FF",
+    X"FF", X"FF", X"FF"
   );
 
   signal w_DIV_FACTOR : unsigned (15 downto 0) := X"0009";
@@ -53,7 +55,6 @@ begin
       w_RST <= '0';
       w_START <= '0';
       wait for period;
-      -- wait for period/2;
       for i in 1 to 30 loop
         w_PIX_IN <= std_logic_vector(to_unsigned(i, w_PIX_IN'length));
         wait for period;
@@ -62,47 +63,52 @@ begin
     end process;
 
   -- p_INPUT : process
-  --   variable v_line : line;
-  --   variable v_data : std_logic_vector(7 downto 0);
+  --   variable v_LINE : line;
+  --   variable v_DATA : std_logic_vector(7 downto 0);
   -- begin
-  --   wait for period/2;
-  --   file_open(fil_in, "img.dat", READ_MODE);
-  --   valid <= '1';
-  --   while not endfile(fil_in) loop
-  --     readline(fil_in, v_LINE);
-  --     read(v_LINE, v_data);
-  --     w_PIX_IN <= v_data;
+  --   wait for period;    
+  --   w_START <= '1';
+  --   wait for period;
+  --   w_RST <= '0';
+  --   w_START <= '0';
+  --   wait for period;
+  --   file_open(f_FILE_IN, "img.dat", READ_MODE);
+  --   while not endfile(f_FILE_IN) loop
+  --     readline(f_FILE_IN, v_LINE);
+  --     read(v_LINE, v_DATA);
+  --     w_PIX_IN <= v_DATA;
   --     wait for period;
   --   end loop;
-  --   valid <= '0';
+  --   wait for period;
+  --   w_RST <= '1';
   --   wait;
   -- end process;
 
   -- p_RESULT : process
-  --   variable v_line : line;
+  --   variable v_LINE : line;
   -- begin
-  --   file_open(fil_out, "img_out.dat", WRITE_MODE);
+  --   file_open(f_FILE_OUT, "img_out.dat", WRITE_MODE);
     
   --   while true loop
   --     wait until rising_edge(w_CLK);
-  --     if valid_o = '1' then
-  --       write(v_line, w_PIX_OUT);
-  --       writeline(fil_out, v_line);
+  --     if w_VALID_PIX = '1' then
+  --       write(v_LINE, w_PIX_OUT);
+  --       writeline(f_FILE_OUT, v_LINE);
   --     end if;
   --   end loop;
   --   wait;
   -- end process;
 
   design_inst : entity work.fir_filter
-  generic map (WINDOW_SIZE => 3, IMAGE_SIZE => 5)
+  generic map (WINDOW_SIZE => 3, IMAGE_SIZE => 100)
   port map (
     i_CLK   => w_CLK,
-    i_RST  => rstn,
+    i_RST  => w_RST,
     i_START     => w_START,
     i_NEW_DATA  => w_PIX_IN,
     i_KERNEL    => w_KERNEL,
     i_DIV_FACTOR => w_DIV_FACTOR,
-    o_VALID_PIX => valid_o,
+    o_VALID_PIX => w_VALID_PIX,
     o_PIX       => w_PIX_OUT
   );
 
